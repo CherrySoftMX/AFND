76b755935e9650ac6d2e5d76d0af68f88b3e6d78
HikingCarrot7 <ricardoibarra2044@gmail.com>
2022-07-31 13:11:18 -0500

    Added insertPreviewNode & removeProviewNode for VisualAutomata class

--- src/main/java/me/hikingcarrot7/afnd/view/components/afnd/VisualAutomata.java
+++ src/main/java/me/hikingcarrot7/afnd/view/components/afnd/VisualAutomata.java
@@ -1,11 +1,17 @@
 package me.hikingcarrot7.afnd.view.components.afnd;
 
+import lombok.RequiredArgsConstructor;
 import me.hikingcarrot7.afnd.core.afnd.AFNDGraph;
 import me.hikingcarrot7.afnd.core.graphs.Node;
+import me.hikingcarrot7.afnd.view.components.afnd.states.AFNDStateFactory;
 
 import java.awt.*;
+import java.util.UUID;
 
+@RequiredArgsConstructor
 public class VisualAutomata extends AFNDGraph<String> {
+  private final AFNDStateFactory stateFactory;
+  private int stateId;
 
   public boolean insertElement(String element, Point pos) {
     boolean elementInserted = insertElement(element);
@@ -16,9 +22,26 @@ public class VisualAutomata extends AFNDGraph<String> {
     return elementInserted;
   }
 
+  public void insertPreviewNode(int stateId, Point pos) {
+    this.stateId = stateId;
+    String element = UUID.randomUUID().toString();
+    boolean elementInserted = insertElement(element, pos);
+    if (elementInserted) {
+      VisualNode visualNode = getVisualNode(element);
+      visualNode.setPreviewNode(true);
+    }
+  }
+
+  public void removePreviewNode() {
+    getNodes().removeIf(node -> {
+      VisualNode visualNode = (VisualNode) node;
+      return visualNode.isPreviewNode();
+    });
+  }
+
   @Override
   protected Node<String> createNode(String element) {
-    return new VisualNode(element);
+    return stateFactory.createState(stateId, element, new Point());
   }
 
   public VisualNode getVisualNode(Object element) {
